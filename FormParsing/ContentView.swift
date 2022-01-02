276ae30939ab0855f3df972789c33142243c30c6
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2022-01-02 18:36:57 -0500

    Initial Commit

---
+++ FormParsing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FormParsing
+//
+//  Created by Christopher Yoon on 1/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FormParsing/FormParsingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormParsingApp.swift
+//  FormParsing
+//
+//  Created by Christopher Yoon on 1/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FormParsingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
