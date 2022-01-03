4a3143f881ba95a37a7ef892711612dfe90601fe
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2022-01-03 14:38:29 -0500

    Program works.  Code cleanup in progress.

--- FormParsing/ContentView.swift
+++ FormParsing/ContentView.swift
@@ -13,17 +13,32 @@ struct ContentView: View {
     @State var empi: String = ""
     @State var reasonForVisit: String = ""
     @State var procedure: String = ""
-    @State var testoutput: [String] = []
+    @State var testoutput = [String]()
+    @State var specificDataOutput = [String]()
     var body: some View {
         VStack {
             HStack {
-                Text("Enter Information Here")
-                Button(action: {
-                    testoutput = processInput(input: inputString)
-                }, label: {
-                    Text("Process")
-                })
+                Text("Enter Information Below")
+                VStack {
+                    Button(action: {
+                        testoutput = processInput(input: inputString)
+                        specificDataOutput = getSpecificData(input: inputString)
+                    }, label: {
+                        Text("Process")
+                    })
+                        .padding()
+                    Button(action: {
+                        testoutput = []
+                    }, label: {
+                        Text("Clear")
+                    })
+                        .padding()
+                    
+                }
                 List(testoutput, id: \.self) { outputLine in
+                    Text(outputLine)
+                }
+                List(specificDataOutput, id: \.self) { outputLine in
                     Text(outputLine)
                 }
             }
@@ -61,14 +76,60 @@ struct ContentView: View {
         components = components.filter(){$0 != ""}
         
         for component in components {
-//            outputArray.append(component)
+            //            outputArray.append(component)
             if component.contains(": ") {
                 var stringArray = component.components(separatedBy: ": ")
                 let category = stringArray.removeFirst()
                 let outputString = stringArray.joined(separator: "")
                 outputArray.append(outputString)
             } else {
-//                outputArray.append("Does Not Contain Info")
+                //                outputArray.append("Does Not Contain Info")
+            }
+        }
+        return outputArray
+    }
+    func getSpecificData(input: String) -> [String] {
+        var outputArray = [String]()
+        var filteredArray = [String]()
+        // Separate by Line Breaks
+        var components = input.components(separatedBy: "\n")
+        // Remove Empty Elements
+        components = components.filter(){$0 != ""}
+        
+        let targetSubstring: [String] = ["Name", "Birth", "Study", "Accession"]
+        for target in targetSubstring {
+            // Cycle through array of targets
+            for component in components {
+                // Cycle through the divided input array
+                if component.contains(target) {
+                    filteredArray.append(component)
+                }
+            }
+        }
+        
+        //
+        for item in filteredArray {
+            if item.contains(": ") {
+                var stringArray = item.components(separatedBy: ": ")
+                _ = stringArray.removeFirst()
+                let outputString = stringArray.joined(separator: "")
+                outputArray.append(outputString)
+            }
+        }
+        
+        
+        return outputArray
+    }
+    
+    private func removeCategory(input: [String]) -> [String] {
+        // Removes characters in string before ": "
+        var outputArray = [String]()
+        for item in input {
+            if item.contains(": ") {
+                var stringArray = item.components(separatedBy: ": ")
+                _ = stringArray.removeFirst()
+                let outputString = stringArray.joined(separator: "")
+                outputArray.append(outputString)
             }
         }
         return outputArray
@@ -77,11 +138,11 @@ struct ContentView: View {
 extension String {
     var mentionedUsernames: [String] {
         let parts = split(separator: "@").dropFirst()
-
+        
         // Character sets may be inverted to identify all
         // characters that are *not* a member of the set.
         let delimiterSet = CharacterSet.letters.inverted
-
+        
         return parts.compactMap { part in
             // Here we grab the first sequence of letters right
             // after the @-sign, and check that it’s non-empty.
