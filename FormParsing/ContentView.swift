0fbad7af4e9c88f4bf37dfc7ceaf09ede672f3e4
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2022-01-02 20:48:39 -0500

    Program separates input multiline string by line break.

--- FormParsing/ContentView.swift
+++ FormParsing/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inputString: String = ""
+    @State var firstName: String = ""
+    @State var empi: String = ""
+    @State var reasonForVisit: String = ""
+    @State var testoutput: [String] = []
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            HStack {
+                Text("Enter Information Here")
+                Button(action: {
+                    testoutput = processInput(input: inputString)
+                }, label: {
+                    Text("Process")
+                })
+                List(testoutput, id: \.self) { outputLine in
+                    Text(outputLine)
+                }
+            }
             .padding()
+            ZStack {
+                TextEditor(text: $inputString)
+                Text(inputString).opacity(0).padding(.all, 8)
+            }
+            .shadow(radius: 1)
+        }
+    }
+    
+    func processInput(input: String) -> [String] {
+        let components = input.components(separatedBy: "\n")
+        
+        return components
+    }
+}
+extension String {
+    var mentionedUsernames: [String] {
+        let parts = split(separator: "@").dropFirst()
+
+        // Character sets may be inverted to identify all
+        // characters that are *not* a member of the set.
+        let delimiterSet = CharacterSet.letters.inverted
+
+        return parts.compactMap { part in
+            // Here we grab the first sequence of letters right
+            // after the @-sign, and check that it’s non-empty.
+            let name = part.components(separatedBy: delimiterSet)[0]
+            return name.isEmpty ? nil : name
+        }
     }
 }
 
